a337ff73b5e5401fd03c77a08636623098f2e44a
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-11-09 14:11:34 +0530

    Authentication

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function DashboardLayout() {
+    return (
+        <div>
+            DashboardLayout
+            <UserButton />
+        </div>
+    )
+}
+
+export default DashboardLayout
