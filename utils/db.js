11725dd5519f3bdb66fd8e493507446e4f9c4562
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-11-09 21:03:36 +0530

    Backend Setup (Drizzle ORM + PostgreSQL)

---
+++ utils/db.js
@@ -0,0 +1,9 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema';
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+export const db = drizzle({ client: sql }, { schema });
